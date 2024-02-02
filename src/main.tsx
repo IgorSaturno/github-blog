9e61a1dbbb9a2d5f0344ec048bd5f440eaa930ef
igor nascimento saturnino <igornascimentosaturnino11@gmail.com>
2024-02-02 18:01:20 -0300

    Alterando endereço do locale

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import 'moment/dist/locale/pt-br'
 import moment from 'moment'
-import 'moment/dist/locale/pt-BR'
 
-moment.locale('pt-BR')
+moment.locale('pt-br')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
